fbbf4ee102a37b7810b2ee65ad5ad0db1afa78d8
Ron Rice <ron31416@gmail.com>
2025-08-31 17:43:50 -0600

    removed title line

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ScoreOSMD from "./ScoreOSMD";
 export default function App() {
   return (
     <main style={{ margin: 24 }}>
-      <h1>Music Viewer v0</h1>
       <ScoreOSMD src="./sample.musicxml" />
     </main>
   );
